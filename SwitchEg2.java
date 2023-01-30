03d9164e5e2b5bb6885c256ac159b14b56f5872a
Codeguyrahul <122788707+Codeguyrahul@users.noreply.github.com>
2023-01-30 18:32:57 +0530

    Add files via upload

---
+++ SwitchEg2.java
@@ -0,0 +1,22 @@
+package ControlStatement;
+
+public class SwitchEg2 {
+public static void main(String[] args) {
+	char ch='a';
+	switch(ch) {
+	case'd':
+		System.out.println("first case excute");
+		break;
+	case'e':
+		System.out.println("second  case excute");
+		break;
+	case'a':
+		System.out.println("thrid case excute");
+		break;
+	case'g':
+		System.out.println("fourth case excute");
+		break;
+		default:System.out.println("ivaild");
+	}
+}
+}
